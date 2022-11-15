b2a964a439dbe9057c14be0412370ea215251534
@regix2003 <reginaldodeoliveirademello@gmail.com>
2022-11-15 12:30:43 -0300

    Projeto Tarefas concluídogit add .!

--- Context/OrganizadorContext.cs
+++ Context/OrganizadorContext.cs
@@ -5,6 +5,7 @@ namespace TrilhaApiDesafio.Context
 {
     public class OrganizadorContext : DbContext
     {
+        // Construtor para manipular/ fazer a ligação com o Banco de Dados
         public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
         {
             
